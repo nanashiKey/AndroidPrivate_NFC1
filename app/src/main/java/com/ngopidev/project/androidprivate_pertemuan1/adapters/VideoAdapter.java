20685da8a77dfd6f628947f0763a6eb71f674c33
Irfan <assidiq.irfan@gmail.com>
2019-07-30 13:40:24 +0700

    update onclick for cardview in adapter

--- app/src/main/java/com/ngopidev/project/androidprivate_pertemuan1/adapters/VideoAdapter.java
+++ app/src/main/java/com/ngopidev/project/androidprivate_pertemuan1/adapters/VideoAdapter.java
@@ -2,12 +2,14 @@ package com.ngopidev.project.androidprivate_pertemuan1.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ngopidev.project.androidprivate_pertemuan1.R;
 import com.ngopidev.project.androidprivate_pertemuan1.models.Video;
@@ -37,10 +39,17 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
     }
 
     @Override
-    public void onBindViewHolder(@NonNull VideoViewHolder videoViewHolder, int i) {
+    public void onBindViewHolder(@NonNull final VideoViewHolder videoViewHolder, int i) {
         Video video = listVideo.get(i);
         videoViewHolder.tv_title.setText(video.getTitles());
         videoViewHolder.tv_subtitle.setText(video.getSubtitle());
+        //memberikan onclick terhadap cardview
+        videoViewHolder.cv_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ctx, "Hai Sample Click " +videoViewHolder.getAdapterPosition(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -51,11 +60,13 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
     public class VideoViewHolder extends RecyclerView.ViewHolder{
         TextView tv_title, tv_subtitle;
         ImageView img_click;
+        CardView cv_main;
         public VideoViewHolder(View itemview){
             super(itemview);
             tv_title = itemview.findViewById(R.id.tv_title);
             tv_subtitle = itemview.findViewById(R.id.tv_subtitle);
             img_click = itemview.findViewById(R.id.img_click);
+            cv_main= itemview.findViewById(R.id.cv_main);
         }
     }
 }
